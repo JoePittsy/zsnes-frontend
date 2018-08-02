16e9a7ef182da7a7649176a4520b91386481ea87
Joe Pitts <joepittsy@gmail.com>
2018-08-02 16:31:42 +0100

    WIP Fav System

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const exec = require('child_process').exec;
-const remote = require('electron').remote
+const remote = require('electron').remote;
+var Datastore = require('nedb')
 var gameRunning = false;
 let w = remote.getCurrentWindow()
 var repGP;
 var dPad = []; //D-PAD
 var leftAno =[]; //Left Analog
-
+var games = [];
+var filtered = false;
+var filterKeyDown;
+var tempGames;
 
 //Runs the ZSNES Emulator and sets the gameRunning flag so we dont steal inuput form ZSNES
 function execute(command) {
@@ -19,18 +23,20 @@ function execute(command) {
 
 
 // Initilizes the carousel of games
-function startSlick(){ 
+async function startSlick(){ 
+    var result = await resolve();
     $(".center").slick({
         accessibility: true,
         arrows: false,
         centerMode:true,
-        slidesToShow: 5,
+        slidesToShow: 3,
         slidesToScroll: 1,
         dots: false,
         infinite: true,
         cssEase: 'linear',
         variableWidth: true,
-        variableHeight: true
+        variableHeight: true,
+
     });
 }
 
@@ -39,26 +45,65 @@ function resolve() {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve('resolved');
-    }, 1);
+    }, 100);
   });
 }
 
-// Loads the games.json file and populates the carousel with images then inits Slick
-async function getGames() {
-    $.getJSON('games.json', function(data) {
-        games = data.games;
-        console.log(games);
-        var slider = $("#gameCar")
-        $("#name").html(games[0][0]);
-        for (var i = 0; i <= games.length; i++) {
-            slider.append("<div><img class = 'gameImage' src = '"+games[i][1]+"'></div>");
-        };
+
+function reloadGames(num){
+    games = []
+    db.loadDatabase(function (err) {    
+        db.find({}, function (err, docs) {
+            for (var i = 0; i < docs.length; i++) {
+                games.push([docs[i]['name'], docs[i]['picPath'], docs[i]['romPath'], docs[i]['favStatus']])
+                if (i == docs.length-1){
+                    if(games[num][3] == 0){
+                        $("#name").html(games[num][0]+" ☆");
+                    } else {
+                        $("#name").html(games[num][0]+" ★");
+                    }
+                }
+            };
+
+        });
+        
     });
-  var result = await resolve();
-  startSlick();
 }
 
-getGames();
+
+
+// Loads the games.json file and populates the carousel with images then inits Slick
+async function getGames() {
+    db = new Datastore({ filename: 'games.db' });
+    db.loadDatabase(function (err) {    // Callback is optional
+        // Find all documents in the collection
+        db.find({}, function (err, docs) {
+            for (var i = 0; i < docs.length; i++) {
+                games.push([docs[i]['name'], docs[i]['picPath'], docs[i]['romPath'], docs[i]['favStatus']])
+            };
+            var slider = $("#gameCar")
+            if(games[0][3] == 0){
+                $("#name").html(games[0][0]+" ☆");
+            } else {
+                $("#name").html(games[0][0]+" ★");
+            }
+            
+            for (var i = 0; i <= games.length; i++) {
+                slider.append("<div><img class = 'gameImage' src = '"+games[i][1]+"'></div>");
+                if (i == games.length-1){
+
+                    startSlick();
+                  
+                } 
+            };                        
+        });
+        
+    });
+
+
+}
+
+
     
 
 // Used to catch keyboard instead of Gamepad
@@ -74,7 +119,6 @@ function checkKey(e) {
     else if (e.keyCode == '13'){
         var num = Number($('#sneaky').html())
         var code = "zsnes "+"'"+__dirname+"/"+games[num][2]+"'"
-        console.log(code)
         execute(code);
     }
 }
@@ -85,6 +129,7 @@ function checkKey(e) {
 function canGame() {
     return "getGamepads" in navigator;
 }
+var keyDown = false
 
 //Grab Gamepad controlls 
 function reportOnGamepad() {
@@ -123,10 +168,66 @@ function reportOnGamepad() {
         }
 
         if (gp.buttons[0].pressed == true){
+            
             var num = Number($('#sneaky').html())
             var code = "zsnes "+"'"+__dirname+"/"+games[num][2]+"'"
-            console.log(code)
             execute(code);
+
+        }
+        if (gp.buttons[1].pressed == true){
+            if (!filterKeyDown){
+                filterKeyDown = true;
+                if (filtered === false) {
+                    $('.center').slick('slickFilter',function(index){
+                        return (games[index][3])
+
+                    });
+                    // $('.center').slick('slickFilter',":even");
+                    tempGames = games
+                    games = games.filter(function(index){
+                        return index[3]
+                    });
+                    filtered = true;
+
+            
+                    // Number($("#sneaky").text())
+                } else {
+                    $('.center').slick('slickUnfilter');
+                    filtered = false;
+                    games = tempGames;
+                
+                }
+                
+            }
+        }
+
+
+        if (gp.buttons[3].pressed == false){
+            keyDown = false;
+        }
+        if (gp.buttons[1].pressed == false){
+            filterKeyDown = false;
+        }
+
+        if (gp.buttons[3].pressed == true){
+            console.log(keyDown)
+            if (keyDown == false){
+                keyDown = true;
+                console.log(keyDown)
+                console.log("Fav!")
+                var num = Number($('#sneaky').html())
+                db.find({ _id: num+1 }, function (err, docs) {
+                    newDoc = docs[0]
+                    console.log(Math.abs(newDoc['favStatus']-1))
+                    db.update({ _id: num+1 }, { $set: {favStatus:Math.abs(newDoc['favStatus']-1)}},{});
+                    reloadGames(num);
+
+
+                  // docs is an array containing documents Mars, Earth, Jupiter
+                  // If no document is found, docs is equal to []
+                });
+
+            }
         }
     }
 }
@@ -134,17 +235,18 @@ function reportOnGamepad() {
 
 //Set up code needed to pair gamepad
 $(document).ready(function() {
+    getGames();
     isFirefox = typeof InstallTrigger !== 'undefined';
     isChrome = !!window.chrome && !!window.chrome.webstore;
 
     if(canGame()) {
 
-        var prompt = "Press any button";
-        $("#gamepadPrompt").text(prompt);
+        
+        $("#gamepadPrompt").html("&nbspDisconnected&nbsp");
 
         $(window).on("gamepadconnected", function() {
             hasGP = true;
-            $("#gamepadPrompt").html("Gamepad connected");
+            $("#gamepadPrompt").html("&nbspConnected&nbsp");
             console.log("connection event");
             gp = navigator.getGamepads()[0];
             repGP = window.setInterval(reportOnGamepad,100);
@@ -152,13 +254,12 @@ $(document).ready(function() {
 
         $(window).on("gamepaddisconnected", function() {
             console.log("disconnection event");
-            $("#gamepadPrompt").text(prompt);
+            $("#gamepadPrompt").html("&nbspDisconnected&nbsp");
             window.clearInterval(repGP);
         });
 
         //setup an interval for Chrome
         var checkGP = window.setInterval(function() {
-            console.log('checkGP');
             if(navigator.getGamepads()[0]) {
                 if(!hasGP) $(window).trigger("gamepadconnected");
                 window.clearInterval(checkGP);
@@ -166,4 +267,12 @@ $(document).ready(function() {
         }, 500);
     }
 
+
 });
+
+
+
+
+
+
+
