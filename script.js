58425cbc8279b7b4c39a4880b72dbf68596228dc
Joe Pitts <joepittsy@gmail.com>
2019-02-03 22:31:08 +0000

    Add games differently

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const exec = require('child_process').exec;
 const remote = require('electron').remote;
+var fs = require('file-system');
 var Datastore = require('nedb');
 let w = remote.getCurrentWindow();
 var gameRunning = false;
@@ -33,28 +34,104 @@ function reloadGames(num) {
         });
     });
 }
+
+
+function restartSlick(){
+    carousel.slick("unslick");
+    carousel.empty();
+    getGames();
+}
+function getDirectories(path) {
+    return fs.readdirSync(path).filter(function (file) {
+        return fs.statSync(path+'/'+file).isDirectory();
+    });
+}
+function buildDB(db, count){ 
+
+
+    folders = getDirectories("SNES/");
+    for (let id = 0; id < folders.length; id++) {
+        const element = folders[id]; 
+
+        db.find({ name: element }, function (err, docs) {
+            console.log(docs);
+            if (docs.length === 0){
+                var game = new Object({
+                    "_id": null,
+                    "name": null,
+                    "picPath": null,
+                    "romPath": null,
+                    "favStatus":null
+                }); 
+                       
+                game["_id"] = id+1+count;
+                game["name"] = element;
+                game["favStatus"] = 0;
+                fs.readdirSync("SNES/" + element).forEach(file => {            
+                    ext = file.split('.').pop();
+                    if ( ext === "sfc" || ext === "smc" ){
+                        // We have a SNES game
+                        game["romPath"] = "SNES/" + element + "/"+ file;               
+                    }
+                    if (ext === "jpg" || ext === "jpeg" || ext === "JPG" || ext == "JPEG"){
+                        // We have a Image game
+                        game["picPath"] = "SNES/" + element + "/"+ file;
+                    }
+                });
+                console.log(game);
+                db.insert(game);
+            }
+        });
+       
+    };    
+}
+
+
 // Loads the games.json file and populates the carousel with images then inits Slick
 async function getGames() {
     db = new Datastore({
         filename: 'games.db'
     });
+    // buildDB();
     db.loadDatabase(function(err) { // Callback is optional
         // Find all documents in the collection
         db.find({}, function(err, docs) {
+            buildDB(db, docs.length);
+        });
+        
+
+        db.find({}, function(err, docs) {
+            console.log(docs.length);
+
             for (var i = 0; i < docs.length; i++) {
                 games.push([docs[i]['name'], docs[i]['picPath'], docs[i]['romPath'], docs[i]['favStatus'], docs[i]['_id']])
             };
-            if (games[0][3] === 0) {
-                $("#name").html(games[0][0] + " ☆");
-            } else {
-                $("#name").html(games[0][0] + " ★");
+            if (docs.length <= 3 ){
+                for (var i = 0; i < docs.length; i++) {
+                    games.push([docs[i]['name'], docs[i]['picPath'], docs[i]['romPath'], docs[i]['favStatus'], docs[i]['_id']])
+                };
+                for (var i = 0; i < docs.length; i++) {
+                    games.push([docs[i]['name'], docs[i]['picPath'], docs[i]['romPath'], docs[i]['favStatus'], docs[i]['_id']])
+                };
             }
-            for (var i = 0; i <= games.length; i++) {
-                carousel.append("<div><img class = 'gameImage' src = '" + games[i][1] + "'></div>");
-                if (i === games.length - 1) {
-                    startSlick();
+            console.log(games);
+            if (games.length !== 0){
+                if (games[0][3] === 0) {
+                    $("#name").html(games[0][0] + " ☆");
+                } else {
+                    $("#name").html(games[0][0] + " ★");
                 }
-            };
+                for (var i = 0; i < games.length; i++) {
+                    console.log(games[i]);
+                    carousel.append("<div><img class = 'gameImage' src = '" + games[i][1] + "'></div>");
+                    if (i === games.length - 1) {
+                        startSlick();
+                    }
+                };
+            } else{
+                console.log("No Games!");
+            }
+            
         });
     });
 };
